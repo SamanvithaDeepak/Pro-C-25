fe5c0ae85f3b6614b6d9463be40edcd6ce19189e
SamanvithaDeepak <78032001+SamanvithaDeepak@users.noreply.github.com>
2021-06-28 18:59:30 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body = Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+
+    display(){
+        push()
+        imageMode(CENTER)
+        ellipse(0,0,this.r,this.r);
+        image(this.image, this.x, this.y, this.r)
+        pop()
+    }
+
+}
